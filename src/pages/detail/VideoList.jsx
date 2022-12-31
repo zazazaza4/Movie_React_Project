3ffb3ab8f9e0477b23e88ac1fad7c32beba80d32
Yaroslav <6715414@gmail.com>
2022-12-31 13:52:44 +0200

    created video list

---
+++ src/pages/detail/VideoList.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import PropTypes from "prop-types";
+
+import tmdbApi from "../../api/tmdbApi";
+
+const VideoList = (props) => {
+  const [videos, setVideos] = useState([]);
+  const { category } = useParams();
+
+  useEffect(() => {
+    const getVideos = async () => {
+      const response = await tmdbApi.getVideos(category, props.id);
+      setVideos(response);
+    };
+    getVideos();
+  }, [category, props.id]);
+
+  return (
+    <>
+      {videos.map((video, index) => (
+        <Video item={video} key={index} />
+      ))}
+    </>
+  );
+};
+
+const Video = ({ item }) => {
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    const height = (iframeRef.current.offsetWidth * 9) / 16 + "px";
+    iframeRef.current.setAttribute("height", height);
+  }, []);
+
+  return (
+    <div className="video">
+      <div className="video__title">
+        <h2>{item.name}</h2>
+      </div>
+      <iframe
+        src={`https://www.youtube.com/embed/${item.key}`}
+        ref={iframeRef}
+        width="100%"
+        title={"trailer"}
+      ></iframe>
+    </div>
+  );
+};
+
+Video.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+VideoList.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default VideoList;
